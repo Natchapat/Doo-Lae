0f8adb1188b839ce404cc55f8d326c9d6dbef95f
Spring-Sama <spring_maprang@hotmail.com>
2015-02-09 23:52:42 +0700

    old maprang

---
+++ app/src/main/java/com/example/spring_sama/doo_lae/Medical.java
@@ -0,0 +1,16 @@
+package com.example.spring_sama.doo_lae;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Spring-Sama on 09-Feb-15.
+ */
+public class Medical extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.medical);
+    }
+}
---
+++ app/src/main/java/com/example/spring_sama/doo_lae/Schedule.java
@@ -0,0 +1,15 @@
+package com.example.spring_sama.doo_lae;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Spring-Sama on 09-Feb-15.
+ */
+public class Schedule extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule);
+    }
+}

07276b7cbb1780e512ad0393e4119e9ad06a6473
Spring-Sama <spring_maprang@hotmail.com>
2015-02-19 22:03:04 +0700

    doolae

---
+++ app/src/main/java/com/example/spring_sama/doo_lae/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.spring_sama.doo_lae;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Spring-Sama on 19-Feb-15.
+ */
+public class Welcome extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wellcome);
+    }
+}
